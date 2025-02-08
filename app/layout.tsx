839cf148f0a84c0e9368d9d7c36b6e4eb7ed9af8
Paul Butler <paul@driftingin.space>
2025-02-08 13:49:35 -0500

    name

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Encode anything as an emoji',
+  title: 'Hide a message in an emoji',
 }
 
 export default function RootLayout({
